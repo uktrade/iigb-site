7bd80de88d08e13cd1495819ce5939c0200d1449
Stuart Axon <stuaxo2@yahoo.com>
2018-01-03 14:45:00 +0000

    Grab the secret key from the environment

--- iigb/settings/production.py
+++ iigb/settings/production.py
@@ -2,6 +2,9 @@ from .heroku_base import *
 
 DEBUG = False
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ["SECRET_KEY"]
+
 try:
     from .local import *
 except ImportError:
