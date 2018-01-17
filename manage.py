377888e7bb01d65cba315887af7134522502088c
Stuart Axon <stuaxo2@yahoo.com>
2018-01-17 14:19:43 +0000

    Remove debug print

--- manage.py
+++ manage.py
@@ -3,8 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-    print(":: SETTINGS MODULE: ", os.environ.get("DJANGO_SETTINGS_MODULE"))
-    print("DATABASE_URL: ", os.environ.get("DATABASE_URL"))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iigb.settings.dev")
     from django.core.management import execute_from_command_line
 
